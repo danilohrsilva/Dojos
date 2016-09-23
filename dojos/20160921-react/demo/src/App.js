82b7ac320e3245c6aafe6eec5d72de9c9fbdc302
Victor Miguez <victorcineis@gmail.com>
2016-09-22 22:21:38 -0300

    React - Adiciona ultimos slides

--- dojos/20160921-react/demo/src/App.js
+++ dojos/20160921-react/demo/src/App.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
+import Apoiador from './Apoiador.js';
+import Busca from './Busca.js';
+import './App.css';
 
 class App extends Component {
   constructor () {
     super();
 
     this.state = {
-      count: 0
+      apoiadores: [],
+      search: ''
     };
 
-    this.increase = this.increase.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
   }
 
-  increase () {
-    this.setState({count: ++this.state.count})
+  setApoiadores () {
+    fetch('http://localhost:8000/apoiadores', { method: 'get' })
+    .then(res => res.json())
+    .then(res => this.setState({apoiadores: res}));
+  }
+
+  handleSearch (e) {
+    this.setState({search: e.target.value})
+  }
+
+  componentWillMount () {
+    this.setApoiadores();
   }
 
   render () {
     return (
-      <header>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.increase}>+</button>
-      </header>
-    )
+      <div className="App">
+        <header>
+          <h1>DOJO - React</h1>
+          <h2>Listagem de apoiadores do <i className="dne">DEV<span className="highlight">na</span>ESTRADA</i></h2>
+        </header>
+        <Busca handleSearch={this.handleSearch}/>
+        <div className="apoiadores">
+          {this.state.apoiadores.map((apoiador) => (
+            apoiador.name.toLowerCase().includes(this.state.search) &&
+            <Apoiador name={apoiador.name} picture={apoiador.picture}/>
+          ))}
+
+          {/*
+           // Outra implementação possível
+
+          {this.state.apoiadores.map((apoiador) => {
+            if (apoiador.name.toLowerCase().includes(this.state.search)) {
+              return <Apoiador name={apoiador.name} picture={apoiador.picture} />
+            }
+          })}
+          */}
+        </div>
+      </div>
+    );
   }
 }
 
