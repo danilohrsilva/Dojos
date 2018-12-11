2966327acd7780e637364e9e6e2b2d02f8f1a32b
Victor Miguez <victorcineis@gmail.com>
2018-12-11 19:56:51 -0200

    Adiciona estilos na lista de ingredientes

--- dojos/20181212-jest-enzyme/src/Components/Ingredient/Ingredient.js
+++ dojos/20181212-jest-enzyme/src/Components/Ingredient/Ingredient.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import './Ingredient.css';
+
 const Ingredient = ({ ingredient, addItem }) => {
   return (
-    <div>
-      <p>{ingredient.name} ${ingredient.price}</p>
-      <button type="button" onClick={() => addItem(ingredient)}>Add</button>
+    <div className="ingredient">
+      <p className="ingredient__info">
+        <span className="ingredient__name">{ingredient.name}</span>
+        <span className="ingredient__price">R${ingredient.price}</span>
+      </p>
+
+      <button type="button" onClick={() => addItem(ingredient)} className="ingredient__button">Adicionar</button>
     </div>
   );
 };
